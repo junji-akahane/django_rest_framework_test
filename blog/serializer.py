e604af359a8f7e5f60208aa55eb1328cb5df3de7
haneya-studio <jjakahane@gmail.com>
2024-04-07 14:12:24 +0900

    relation

--- blog/serializer.py
+++ blog/serializer.py
@@ -7,7 +7,18 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('name', 'mail')
 
 class EntrySerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+    author_id = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), write_only=True)
     class Meta:
         model = Entry
-        fields = ('title', 'body', 'created_at', 'status', 'author')
+        fields = ('title', 'body', 'created_at', 'status', 'author', 'author_id')
 
+    def create(self, validated_data):
+        validated_data['author'] = validated_data.get('author_id', None)
+
+        if validated_data['author'] is None:
+            raise serializers.ValidationError("user not found.") 
+
+        del validated_data['author_id']
+
+        return Entry.objects.create(**validated_data)
