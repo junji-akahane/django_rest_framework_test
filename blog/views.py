21fd4d9703a916a85c4c034ce65776290d7903d1
haneya-studio <jjakahane@gmail.com>
2024-04-07 14:15:55 +0900

    filter

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets, filters
+import django_filters
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets
 
 from .models import User, Entry
 from .serializer import UserSerializer, EntrySerializer
@@ -7,6 +9,16 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+class EntryFilter(django_filters.FilterSet):
+    author = django_filters.NumberFilter()
+    status = django_filters.CharFilter()
+
+    class Meta:
+        model = Entry
+        fields = ('author', 'status')
+
 class EntryViewSet(viewsets.ModelViewSet):
     queryset = Entry.objects.all()
     serializer_class = EntrySerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = EntryFilter
